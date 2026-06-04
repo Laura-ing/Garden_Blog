af60de81ea7f607e53a29e04a4d3854af95f9c23
Laura-ing <anamarialaura.dinca@gmail.com>
2026-06-05 00:07:41 +0300

    Fix hero background position

--- js/blog.js
+++ js/blog.js
@@ -118,8 +118,7 @@ function initArticlePage() {
     </div>`;
 
   container.innerHTML = `
-    <div class="article-hero" style="background-image:url('${article.image}'); background-position: center; background-size: cover;">
-      <div class="article-hero-overlay"></div>
+      <div class="article-hero" style="background-image:url('${article.image}'); background-position: center center; background-size: cover; background-repeat: no-repeat;">      <div class="article-hero-overlay"></div>
       <div class="article-hero-content">
         <p class="section-label">${article.category}</p>
         <h1 class="article-title">${article.title}</h1>
